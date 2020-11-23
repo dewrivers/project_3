41d372629b287b6489009d1c75e7afadcf8252f2
Rocio Rios <rrocio46@yahoo.es>
2020-11-23 15:26:51 -0500

    fix the front for the authentication

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,60 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Axios from "axios";
-import Header from './components/layouts/Header';
-import Home from './components/pages/Home';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import UserContext from '../src/context/UserContext';
+import Header from "./components/layouts/Header";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
-import './style.css';
+import "./style.css";
 
-function App() {
- const [ userData, setUserData ] = useState({
-     token: undefined,
-     user: undefined,
- });
-  
+export default function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
   useEffect(() => {
     const checkLoggedIn = async () => {
-      let token = localStorage.getItem('auth-token');
-      if (token === null) { 
-      localStorage.setItem('auth-token', '');
-      token = '';
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
       }
-
       const tokenRes = await Axios.post(
-        'http://localhost:7777/users/tokenIsValid', 
+        "http://localhost:7777/users/tokenIsValid",
         null,
-        { headers: {'x-auth-token': token }}
+        { headers: { "x-auth-token": token } }
       );
-      if(tokenRes.data){
-        const userRes = await Axios.get('http://localhost:7777/users/', {
-         headers: {'x-auth-token': token }
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:7777/users/", {
+          headers: { "x-auth-token": token },
         });
         setUserData({
           token,
-          user: userRes.data
+          user: userRes.data,
         });
       }
     };
+
     checkLoggedIn();
   }, []);
 
   return (
-  <>
-  <BrowserRouter>
-   <UserContext.Provider value={{ userData, setUserData }}>
-    <Header />
-     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-   </UserContext.Provider>
-  </BrowserRouter>
-  </>
+    <>
+      <BrowserRouter>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </div>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
